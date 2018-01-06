fad148fd5b5729e48b8e7f48a72c20929e9f0a45
Club User 1 <case@blackalchemy.to>
2018-01-05 18:15:03 -0800

    .

---
+++ EchoCalibrate.js
@@ -0,0 +1,22 @@
+var five = require("johnny-five");
+var board = new five.Board();
+
+board.on("ready", function() {
+  var proximity = new five.Proximity({
+    controller: "HCSR04",
+    pin: 2
+  });
+
+  proximity.on("data", function() {
+    console.log('\033c');
+	console.log("Proximity: ");
+    console.log("  cm  : ", this.cm);
+    console.log("  in  : ", this.in);
+    console.log("-----------------");
+  });
+
+  proximity.on("change", function() {
+    console.log("The obstruction has moved.");
+  });
+
+});
