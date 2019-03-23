014cee964b8507fa96ba9bf3289c80c17000f565
Cayce Beames <case@blackalchemy.to>
2019-03-22 19:55:39 -0700

    adding sumo to motor controlled bot

--- Sumo-Motor/Sumo-Motor.ino
+++ Sumo-Motor/Sumo-Motor.ino
@@ -2,14 +2,14 @@
 
 // Behavior indicator LED
 // This tells us when to calibrate, when the robot is ready to run and that it's running it's program
-const int LEDpin = 7; 
+const int LEDpin = 7;
 
 // Distance sensor connections
 const int frontEchoPin = 9;
 const int frontTriggerPin = 8;
 
 // Distance sensor settings
-volatile float maxFrontDistance = 5.00;
+volatile float maxFrontDistance = 12;
 volatile float frontDuration, frontDistanceCm;
 
 // digital write will receive the full 5v signal.  This may be too fast.
@@ -27,12 +27,14 @@ int stop = 0;
 
 // Reflectivity sensor connection and settings
 // sensors 0 through 5 are connected to analog inputs 0 through 5, respectively
-#define NUM_SENSORS             6  // number of sensors used
+// in our example, we're only using 4 sensors on A0-A3
+// we are using sensors 1, 3, 6, and 8 on the QTR8A
+#define NUM_SENSORS             4  // number of sensors used
 #define NUM_SAMPLES_PER_SENSOR  4  // average 4 analog samples per sensor reading
 #define EMITTER_PIN             2  // emitter is controlled by digital pin 2
 #define R_THRESHOLD           900  // The threshold beyond which the sensor reads a line
 
-QTRSensorsAnalog qtra((unsigned char[]) {0, 1, 2, 3, 4, 5}, 
+QTRSensorsAnalog qtra((unsigned char[]) {0, 1, 2, 3},
   NUM_SENSORS, NUM_SAMPLES_PER_SENSOR, EMITTER_PIN);
 
 unsigned int sensorValues[NUM_SENSORS];
@@ -40,8 +42,6 @@ unsigned int s0;
 unsigned int s1;
 unsigned int s2;
 unsigned int s3;
-unsigned int s4;
-unsigned int s5;
 unsigned int sensorMinAvg;
 unsigned int sensorMaxAvg;
 unsigned int sensorMinSum;
@@ -93,7 +93,7 @@ void setup() {
   pinMode(left_wheel_back, OUTPUT);
   pinMode(right_wheel, OUTPUT);
   pinMode(right_wheel_back, OUTPUT);
-  
+
   delay(5000);
 }
 
@@ -110,7 +110,7 @@ void loop() {
   // when 0, the line is to the right of the robot
   // when 5000, the line is to the left of the robot
   // any number in between, the robot is on the line over one of the sensors
-  // 
+  //
   //  Serial.println(position);
 
   //  while ( 0 < position < 5000)  {
@@ -119,7 +119,7 @@ void loop() {
   // not complete, more testing to be done.
   //  ------------------- End Line Following --------
 
-  //  ------------------- Sumo ----------------------  
+  //  ------------------- Sumo ----------------------
   // front distance check
   checkFrontDistance();
   do {
@@ -129,11 +129,9 @@ void loop() {
     s1 = sensorValues[1];
     s2 = sensorValues[2];
     s3 = sensorValues[3];
-    s4 = sensorValues[4];
-    s5 = sensorValues[5];
-       
+
     // prints the values from the sensor to determine the position of the line.
-    // 
+    //
     Serial.print(s0);
     Serial.print(" ");
     Serial.print(s1);
@@ -141,35 +139,32 @@ void loop() {
     Serial.print(s2);
     Serial.print(" ");
     Serial.print(s3);
-    Serial.print(" ");
-    Serial.print(s4);
-    Serial.print(" ");
-    Serial.print(s5);
     Serial.println();
 
     Serial.println("Searching...");
-    moveLeft();
-    
-    } while (frontDistanceCm < maxFrontDistance && s0 > R_THRESHOLD && s5 > R_THRESHOLD);
 
     if (frontDistanceCm < maxFrontDistance) {
       attack();
+    } else {
+      moveForward();
     }
 
-    if (s0 > R_THRESHOLD || s5 > R_THRESHOLD) {
-      Serial.println("Line detected! Back Up!");
-      moveBackward();
-    }
+   } while (s0 > R_THRESHOLD || s3 > R_THRESHOLD);
+
+   moveBackward();
+   delay(500);
+   moveRight();
+   delay(400);
 
 
-  
+
   //  ----------------- End Sumo  --------------------
 } // end void loop
 
 void checkFrontDistance() {
-  digitalWrite(frontTriggerPin, LOW);  
+  digitalWrite(frontTriggerPin, LOW);
   delayMicroseconds(4);
-  digitalWrite(frontTriggerPin, HIGH);  
+  digitalWrite(frontTriggerPin, HIGH);
   delayMicroseconds(10);
   digitalWrite(frontTriggerPin, LOW);
   frontDuration = pulseIn(frontEchoPin, HIGH);  // We measure the time between pulses, in microseconds
